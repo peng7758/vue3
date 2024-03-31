0c71334b460350cf616ef8c68955c9edfcb516b8
pengguanghui <1091990407@qq.com>
2024-03-31 11:54:24 +0800

    基本框架

--- xxx/src/App.vue
+++ xxx/src/App.vue
@@ -1,26 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="vartbom">
+    <IndexApp></IndexApp>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import IndexApp from './components/index.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    IndexApp
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  background-color: #000;
+}
+#vartbom {
+  position: fixed;
+  bottom: 0;
+  height: 200px;
+  border: 1px solid #8d8787;
+  width: 100%;
+  box-shadow: 0px -1px red;
+  background-color: #fff;
+  margin-left: -10px;
 }
 </style>
